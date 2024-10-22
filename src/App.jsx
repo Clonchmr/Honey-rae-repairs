71efc1a98d82f576aeeccff2d1e0052fc139d1e9
Matthew Clonch <matthewrclonch@gmail.com>
2024-10-22 13:48:11 -0500

    added in customer list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./app.css"
+import { CustomerList } from "./Components/customers/CustomersList"
 import { TicketList } from "./Components/tickets/TicketList"
 import "./index.css"
 
 export const App = () => {
   return (
     <>
-      <TicketList/>
+      {/*<TicketList/>*/}
+      <CustomerList />
     </>
   )
 }
---
+++ src/users/User.jsx
@@ -0,0 +1,23 @@
+import "./users.css"
+export const User = ({ user }) => {
+   return ( 
+    <div key={user.id} className="user">
+        <div>
+        <div className="user-info">
+                Name
+            </div>
+                <div>
+                     {user.fullName}
+                </div>
+            </div>
+            <div>
+            <div className="user-info">
+                    Email
+            </div>
+                <div>
+                    {user.email}
+                </div>
+            </div>
+        </div>
+                
+)}
